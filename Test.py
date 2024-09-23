181d61846fb1cf3b80eb3b2500a8dbd58a75b941
Shrestha <120598649+shreshhhh@users.noreply.github.com>
2024-09-23 20:00:27 +0530

    Initial commit

---
+++ Test.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# Load the saved model
+model = tf.keras.models.load_model('ECG.h5')
+
+# Define the test data generator
+test_datagen = ImageDataGenerator(rescale=1./255)
+x_test = test_datagen.flow_from_directory("C:/Users/vicky/OneDrive/Documents/Image based Heartbeat classification for Arrhythmia Detection Using CNN/data/test", target_size=(64, 64), batch_size=32, class_mode="categorical", shuffle=False)
+
+
+# Generate predictions for the test set
+y_pred = model.predict(x_test, steps=len(x_test), verbose=1)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+# Get true labels
+y_true = x_test.classes
+
+# Sort class labels based on their indices
+class_labels = [k for k, v in sorted(x_test.class_indices.items(), key=lambda item: item[1])]
+
+# Generate confusion matrix
+conf_matrix = confusion_matrix(y_true, y_pred_classes)
+
+# Plot the confusion matrix
+plt.figure(figsize=(10, 8))  # Adjust the figure size as needed
+sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=class_labels, yticklabels=class_labels)
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.xticks(rotation=45)  # Rotate x-axis labels
+plt.yticks(rotation=0)   # Rotate y-axis labels
+plt.rc('font', size=8)  # Adjust font size
+plt.tight_layout()  # Ensures the plot fits within the figure area
+plt.show()
+
+# Print classification report
+print(classification_report(y_true, y_pred_classes, target_names=class_labels))
---
+++ Train.py
@@ -0,0 +1,57 @@
+from keras.models import Sequential
+from keras.layers import Dense, Convolution2D, MaxPooling2D, Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Image data generators
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+x_train = train_datagen.flow_from_directory("C:/Users/vicky/OneDrive/Documents/Image based Heartbeat classification for Arrhythmia Detection Using CNN/data/train", target_size=(64, 64), batch_size=32, class_mode="categorical")
+x_test = test_datagen.flow_from_directory("C:/Users/vicky/OneDrive/Documents/Image based Heartbeat classification for Arrhythmia Detection Using CNN/data/test", target_size=(64, 64), batch_size=32, class_mode="categorical")
+
+# Model building
+model = Sequential()
+model.add(Convolution2D(32, (3, 3), input_shape=(64, 64, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(units=128, kernel_initializer="random_uniform", activation="relu"))
+model.add(Dense(units=128, kernel_initializer="random_uniform", activation="relu"))
+model.add(Dense(units=128, kernel_initializer="random_uniform", activation="relu"))
+model.add(Dense(units=128, kernel_initializer="random_uniform", activation="relu"))
+model.add(Dense(units=128, kernel_initializer="random_uniform", activation="relu"))
+model.add(Dense(units=6, kernel_initializer="random_uniform", activation="softmax"))
+
+model.summary()
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Training the model
+history = model.fit_generator(generator=x_train, steps_per_epoch=len(x_train), epochs=9, validation_data=x_test, validation_steps=len(x_test))
+
+# Plotting the training and validation loss and accuracy
+sns.set()
+fig = plt.figure(0, (12, 4))
+
+ax = plt.subplot(1, 2, 1)
+sns.lineplot(x=history.epoch, y=history.history['accuracy'], label='train')
+sns.lineplot(x=history.epoch, y=history.history['val_accuracy'], label='valid')
+plt.title('Accuracy')
+plt.tight_layout()
+
+ax = plt.subplot(1, 2, 2)
+sns.lineplot(x=history.epoch, y=history.history['loss'], label='train')
+sns.lineplot(x=history.epoch, y=history.history['val_loss'], label='valid')
+plt.title('Loss')
+plt.tight_layout()
+
+plt.savefig('epoch_history_dcnn.png')
+plt.show()
+
+# Saving the model
+model.save('ECG.h5')
